ef0241494868c8efbd31ebd13cf64f081040f1e3
John Cardiff-Winchell <johnwinchell86@fullsail.edu>
2013-03-04 23:55:50 -0500

    added switch case function

--- main.js
+++ main.js
@@ -41,12 +41,50 @@ function dropDown2(){
 		}		
 		pickLi.appendChild(createSelect)
 }
+
+//Find value of selected radio button
+function getRadio(){
+	var rad = document.forms[0].newUsed;
+		for(var i=0; i<rad.length; i++){
+			if(rad[i].checked){
+				newUsedValue = rad[i].value;
+			}
+		}
+}
+
+// Making switch case function to toggle links
+
+function toggle(x){
+	switch(x){
+		case "on":
+			gid("vehLib").style.display = "none";
+			gid("clear").style.display = "inline";
+			gid("display").style.display = "none";
+			gid("addCar").style.display = "inline";
+			break;
+		case "off":
+			gid("vehLib").style.display = "block";
+			gid("clear").style.display = "inline";
+			gid("display").style.display = "inline";
+			gid("addCar").style.display = "none";
+			gid("item").style.display = "none";
+			break;
+		default:
+			break;
+			return false;
+		}
+	
+}
+
 	
 // Variable Defaults
 var makeList = ["--Choose a make--", "Chevrolet", "Ford", "Buick", "Dodge", "Jeep"],
 	modelList = ["--Choose a model--", "Cruze", "Impala", "Cobalt", "Camaro", "Focus", "Explorer", "Fusion", "F-150", "Regal", "Lacrosse", "Enclave", "Century", "Viper", "Ram", "Dart", "Durango", "Wrangler", "Liberty", "Patriot", "Compass"];
 dropDown1();
 dropDown2();
+
+
+
 // Links and submit button
 var display = gid("display");
 display.addEventListener("click", getData);
