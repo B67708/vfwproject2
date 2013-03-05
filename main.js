65c4c467552b40796932412abc4ded960fe5e73f
John Cardiff-Winchell <johnwinchell86@fullsail.edu>
2013-03-04 23:30:44 -0500

    added dropdown and array variables

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function gid(x){
 	var element = document.getElementById(x);
 	return element;
 }
-// Create forms for drop down menu.
+// Creates form for make drop down menu.
 function dropDown1(){
 	var formTag = document.getElementsByTagName("form"),
 		pickLi = gid("make"),
@@ -26,11 +26,27 @@ function dropDown1(){
 		pickLi.appendChild(createSelect)
 }	
 	
+// Creates form for model drop down menu.
+function dropDown2(){
+	var formTag = document.getElementsByTagName("form"),
+		pickLi = gid("model"),
+		createSelect = document.createElement("select");
+		createSelect.setAttribute("id", "model");
+		for(var i=0, j=modelList.length; i<j; i++){
+			var makeOption = document.createElement("option");
+			var optionTxt = modelList[i];
+			makeOption.setAttribute("value", optionTxt);
+			makeOption.innerHTML = optionTxt;
+			createSelect.appendChild(makeOption);
+		}		
+		pickLi.appendChild(createSelect)
+}
 	
 // Variable Defaults
-var makeList = ["--Choose a make--", "Chevrolet", "Ford", "Buick", "Dodge", "Jeep"];
+var makeList = ["--Choose a make--", "Chevrolet", "Ford", "Buick", "Dodge", "Jeep"],
+	modelList = ["--Choose a model--", "Cruze", "Impala", "Cobalt", "Camaro", "Focus", "Explorer", "Fusion", "F-150", "Regal", "Lacrosse", "Enclave", "Century", "Viper", "Ram", "Dart", "Durango", "Wrangler", "Liberty", "Patriot", "Compass"];
 dropDown1();
-
+dropDown2();
 // Links and submit button
 var display = gid("display");
 display.addEventListener("click", getData);
@@ -42,6 +58,7 @@ var submit = gid("submit");
 submit.addEventListener("click", submitData);
 
 
+	
 
 
 })
